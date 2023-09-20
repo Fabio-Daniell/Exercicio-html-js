820616289cb3d3d94b1bf4a2344057af4dd14101
Fabio-Daniell <141465125+Fabio-Daniell@users.noreply.github.com>
2023-09-19 21:23:38 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,58 @@
+const form = document.getElementById('articles');
+const valorA = document.getElementById('valor-A');
+const valorB = document.getElementById('valor-B');
+let formEValido = false; 
+
+function validaValor(valorx,valory){
+    var Valida = valorx < valory;
+    return Valida;
+}
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const messageError = document.querySelector('.error-messanger');
+    messageError.style.display = 'none';
+    valorA.style.outlineColor = 'black';
+    valorA.style.border = 'none' ;
+    valorA.style.color = 'black';
+
+    const mensadeSuccesso = document.querySelector('.mensager-successo');
+    mensadeSuccesso.style.display = 'none';
+
+    formEValido = validaValor(valorA.value,valorB.value);
+    if(formEValido){
+        mensadeSuccesso.style.display = 'block';
+
+        valorA.value = '';
+        valorB.value = '';
+    }else{
+        messageError.style.display = 'block';
+        valorA.style.outlineColor = 'red';
+        valorA.style.border = '2px solid red' ;
+        valorA.style.color = 'red';
+    }
+})
+
+form.addEventListener('keyup', function(e){
+    console.log(e.target.velue);
+
+    const messageError = document.querySelector('.error-messanger');
+    messageError.style.display = 'none';
+    valorA.style.outlineColor = 'black';
+    valorA.style.border = 'none' ;
+    valorA.style.color = 'black';
+
+    const mensadeSuccesso = document.querySelector('.mensager-successo');
+    mensadeSuccesso.style.display = 'none';
+
+    formEValido = validaValor(valorA.value,valorB.value);
+    if(formEValido){
+        mensadeSuccesso.style.display = 'none';
+    }else{
+        messageError.style.display = 'block';
+        valorA.style.outlineColor = 'red';
+        valorA.style.border = '2px solid red' ;
+        valorA.style.color = 'red';
+    }
+})
